eeb4c2055daaaa5e6a60d5b19b606850d860065d
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-07-12 20:11:07 +0545

    fetched data from provided API

--- programiz-assignment/src/app/page.tsx
+++ programiz-assignment/src/app/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Header from "@/components/Header";
+import axios from "axios";
+
+const fetchData = async () => {
+  const url =
+    "https://storage.googleapis.com/programiz-static/hiring/software/job-listing-page-challenge/data.json";
+  const res = await axios.get(url);
+  return res.data;
+};
 
 export default function Home() {
+  const [state, setState] = useState<"loading" | "done" | null>(null);
+  const [data, setData] = useState<null | [{}]>();
+  useEffect(() => {
+    setState("loading");
+    fetchData()
+      .then((res) => {
+        setData(res);
+        console.log(res);
+      })
+      .catch((e: Error) => console.log(e));
+  }, []);
   return (
     <div>
       <Header />
