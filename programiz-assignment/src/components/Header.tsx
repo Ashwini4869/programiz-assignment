adef939e373c334b4b400adde15227d76d6e2545
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-07-12 19:31:00 +0545

    completed basic header section and theme

---
+++ programiz-assignment/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="h-16 bg-teal-600 text-slate-50 flex justify-center">
+      <div className="mt-5 font-serif tracking-wider">
+        Welcome to Jobs Listing Page
+      </div>
+    </div>
+  );
+};
+
+export default Header;
